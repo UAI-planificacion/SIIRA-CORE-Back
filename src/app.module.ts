6ba34ea7982ae26e2313791b51be9962bc2a7bf7
kevinkeyssx <keyss1700@gmail.com>
2026-07-27 22:18:16 -0400

    PeriodsModule & ProcessConfigsModule implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SeedModule } from './seed/seed.module';
+import { PeriodsModule } from './periods/periods.module';
+import { ProcessConfigsModule } from './process-configs/process-configs.module';
 
 import { AppController }    from './app.controller';
 import { PrismaModule }     from './prisma/prisma.module';
 
 
 @Module({
-    imports         : [PrismaModule, SeedModule],
+    imports         : [PrismaModule, SeedModule, PeriodsModule, ProcessConfigsModule],
     controllers     : [AppController],
     providers       : [],
 })
