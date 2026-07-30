582c870d55857fca9e5764998aed04a0841a0c63
kevinkeyssx <keyss1700@gmail.com>
2026-07-29 21:59:52 -0400

    StudyPlanModule implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
-import { SeedModule } from './seed/seed.module';
-import { PeriodsModule } from './periods/periods.module';
-import { ProcessConfigsModule } from './process-configs/process-configs.module';
 
-import { AppController }    from './app.controller';
-import { PrismaModule }     from './prisma/prisma.module';
+import { AppController }        from './app.controller';
+import { PrismaModule }         from './prisma/prisma.module';
+import { SeedModule }           from './seed/seed.module';
+import { PeriodsModule }        from './periods/periods.module';
+import { ProcessConfigsModule } from './process-configs/process-configs.module';
+import { StudyPlanModule }      from './study-plan/study-plan.module';
 
 
 @Module({
-    imports         : [PrismaModule, SeedModule, PeriodsModule, ProcessConfigsModule],
+    imports         : [
+        PrismaModule,
+        SeedModule,
+        PeriodsModule,
+        ProcessConfigsModule,
+        StudyPlanModule
+    ],
     controllers     : [AppController],
     providers       : [],
 })
