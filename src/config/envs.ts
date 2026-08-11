472c727deb53f14a29da0bb2297f9509508d3487
kevinkeyssx <keyss1700@gmail.com>
2026-08-10 22:46:36 -0400

    Redis envs implemented

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,16 +3,24 @@ import * as joi from 'joi';
 
 
 interface EnvVars {
-    PORT            : number;
-    DATABASE_URL    : string;
-    ALLOWED_ORIGINS : string;
+	PORT            : number;
+	DATABASE_URL    : string;
+	ALLOWED_ORIGINS : string;
+	REDIS_HOST      : string;
+	REDIS_PORT      : number;
+	REDIS_PASSWORD  : string;
+	REDIS_TLS       : boolean;
 }
 
 
 const envsSchema = joi.object({
-    PORT            : joi.number().required(),
-    DATABASE_URL    : joi.string().required(),
-    ALLOWED_ORIGINS : joi.string().required(),
+	PORT            : joi.number().required(),
+	DATABASE_URL    : joi.string().required(),
+	ALLOWED_ORIGINS : joi.string().required(),
+	REDIS_HOST      : joi.string().required(),
+	REDIS_PORT      : joi.number().default( 6379 ),
+	REDIS_PASSWORD  : joi.string().allow( '' ).default( '' ),
+	REDIS_TLS       : joi.boolean().default( false ),
 })
 .unknown( true );
 
@@ -27,7 +35,14 @@ const envVars: EnvVars = value;
 
 
 export const ENVS = {
-    PORT              : envVars.PORT,
-    DATABASE_URL      : envVars.DATABASE_URL,
-    ALLOWED_ORIGINS   : envVars.ALLOWED_ORIGINS.split( ',' ),
+	PORT            : envVars.PORT,
+	DATABASE_URL    : envVars.DATABASE_URL,
+	ALLOWED_ORIGINS : envVars.ALLOWED_ORIGINS.split( ',' ),
+
+    REDIS : {
+        HOST      : envVars.REDIS_HOST,
+        PORT      : envVars.REDIS_PORT,
+        PASSWORD  : envVars.REDIS_PASSWORD,
+        TLS       : envVars.REDIS_TLS,
+    }
 }
