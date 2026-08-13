8f7632e5e69a15553a4378e6684da99afb2c25a0
kevinkeyssx <keyss1700@gmail.com>
2026-08-12 21:25:30 -0400

    SseModule imported

--- src/study-plan/study-plan.module.ts
+++ src/study-plan/study-plan.module.ts
@@ -2,10 +2,11 @@ import { Module }              from '@nestjs/common';
 import { StudyPlanService }    from './study-plan.service';
 import { StudyPlanController } from './study-plan.controller';
 import { QueueModule }         from '@queue/queue.module';
+import { SseModule }           from '@sse/sse.module';
 
 
 @Module( {
-	imports     : [ QueueModule ],
+	imports     : [ QueueModule, SseModule ],
 	controllers : [ StudyPlanController ],
 	providers   : [ StudyPlanService ],
 } )
